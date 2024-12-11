a971bf0829d94abfa05419df0e509fc40e8ec3d6
Rojen Dangol <rojendangol44@gmail.com>
2024-12-11 11:56:41 +0545

    Login n Register

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -326,7 +326,7 @@
             </a>
 
             <div class="logo">
-                <a href="index.html">
+                <a href="{{route('home.index')}}">
                     <img
                         src="{{asset('assets/images/logo.png') }}"
                         alt="Uomo"
@@ -408,7 +408,7 @@
                         class="navigation__list list-unstyled position-relative"
                     >
                         <li class="navigation__item">
-                            <a href="index.html" class="navigation__link"
+                            <a href="{{route('home.index')}}" class="navigation__link"
                                 >Home</a
                             >
                         </li>
@@ -539,7 +539,7 @@
         <div class="container">
             <div class="header-desk header-desk_type_1">
                 <div class="logo">
-                    <a href="index.html">
+                    <a href="{{route('home.index')}}">
                         <img
                             src="{{asset('assets/images/logo.png') }}"
                             alt="Uomo"
@@ -551,7 +551,7 @@
                 <nav class="navigation">
                     <ul class="navigation__list list-unstyled d-flex">
                         <li class="navigation__item">
-                            <a href="index.html" class="navigation__link"
+                            <a href="{{route('home.index')}}" class="navigation__link"
                                 >Home</a
                             >
                         </li>
@@ -692,21 +692,39 @@
                         </div>
                     </div>
 
+                    @guest  
                     <div class="header-tools__item hover-container">
-                        <a href="login.html" class="header-tools__item">
+                        <a href="{{route('login')}}" class="header-tools__item">
                             <svg
+                            class="d-block"
+                            width="20"
+                            height="20"
+                            viewBox="0 0 20 20"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                            >
+                            <use href="#icon_user" />
+                            </svg>
+                        </a>
+                    </div>
+                    @else
+                        <div class="header-tools__item hover-container">
+                            <a href="{{ Auth::user()->utype === 'ADM' ? route('admin.index'): route('user.index') }}" class="header-tools__item">
+                                <span class="pr-6px">{{ Auth::user()->name }}</span>
+                                <svg
                                 class="d-block"
                                 width="20"
                                 height="20"
                                 viewBox="0 0 20 20"
                                 fill="none"
                                 xmlns="http://www.w3.org/2000/svg"
-                            >
+                                >
                                 <use href="#icon_user" />
-                            </svg>
-                        </a>
-                    </div>
-
+                                </svg>
+                            </a>
+                        </div>
+                    @endguest
+                
                     <a href="wishlist.html" class="header-tools__item">
                         <svg
                             width="20"
@@ -753,7 +771,7 @@
                     class="footer-column footer-store-info col-12 mb-4 mb-lg-0"
                 >
                     <div class="logo">
-                        <a href="index.html">
+                        <a href="{{route('home.index')}}">
                             <img 
                                 src="{{asset('assets/images/logo.png') }}"
                                 alt="SurfsideMedia"
@@ -1014,7 +1032,7 @@
         <div class="row text-center">
             <div class="col-4">
                 <a
-                    href="index.html"
+                    href="{{route('home.index')}}"
                     class="footer-mobile__link d-flex flex-column align-items-center"
                 >
                     <svg
@@ -1033,7 +1051,7 @@
 
             <div class="col-4">
                 <a
-                    href="index.html"
+                    href="{{route('home.index')}}"
                     class="footer-mobile__link d-flex flex-column align-items-center"
                 >
                     <svg
@@ -1052,7 +1070,7 @@
 
             <div class="col-4">
                 <a
-                    href="index.html"
+                    href="{{route('home.index')}}"
                     class="footer-mobile__link d-flex flex-column align-items-center"
                 >
                     <div class="position-relative">
